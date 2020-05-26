1128d9d7235898c0a345e9b06a78ce4e4b836534
Aniket <mit.aniket98@gmail.com>
2020-05-26 21:10:31 +0530

    comment added

--- PredictSal.R
+++ PredictSal.R
@@ -1,23 +1,27 @@
 #install.packages("caTools")
 #install.packages("ggplot2")
+#install.packages("gridExtra")
 library(ggplot2)
 library(caTools)
 library(gridExtra)
 
-
+#Importing Data from Local storage
 dataset<-read.csv("Predicting_Salaries.csv",header = T)
 
+#setting seed to get same split on different execution
 set.seed(123)
+
+#splitting in 3/4 ratio.
 sample<-sample.split(dataset$AnnualSalary,SplitRatio = .75)
 data_train<-subset(dataset,sample == T)
 data_test<-subset(dataset,sample == F)
 
-
+#AnnualSalary -> Dependent variable 
+#Years of exp. -> Independent variable
 linearRegg<-lm(AnnualSalary~YearsOfExperience,data = data_train)
 
+#Predicting with test data
 test_predict<-predict(linearRegg,newdata=data_test)
-
-
 
 
 
@@ -39,4 +43,5 @@ Test.plot<- ggplot()+
               scale_x_continuous(limits = c(0,12))+
               scale_y_continuous(limits = c(0,150000))
 
+#To dispay both plots
 gridExtra::grid.arrange(Train.plot,Test.plot,ncol=2)
